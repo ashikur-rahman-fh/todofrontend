9e3a3304b46f0d6b97ea42c5fc697fc7460ecf05
ashikur-rahman-fh <ashikurrahmanvar@gmail.com>
2022-11-05 08:36:13 -0400

    Add delete confirmation

--- src/components/Todo/ViewTodos/TodoListItem/constants.js
+++ src/components/Todo/ViewTodos/TodoListItem/constants.js
@@ -2,3 +2,7 @@ export const UPDATE_REQUST_CONFIG = {
     METHOD: `PATCH`,
     ULR: (id) => `api/todos/${id}`,
 };
+
+export const DELETE_TODO_CONFIRM_MESSAGE = (todo) => {
+    return `Do you want to delete "${todo?.title}"`;
+};
